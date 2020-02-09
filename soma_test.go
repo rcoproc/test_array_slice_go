851763b2dc9937d500155f3689d3e407155d305f
Ricardo César de Oliveira <rioliveira@bionexo.com>
2020-02-09 02:38:23 -0300

    Added method SomaTudo

--- soma_test.go
+++ soma_test.go
@@ -1,18 +1,10 @@
 package main
 
 import "testing"
+import "reflect"
 
 func TestSoma(t *testing.T) {
-	t.Run("coleção de 5 números", func(t *testing.T) {
-		numeros := []int{1, 2, 3, 4, 5}
-
-		resultado := Soma(numeros)
-		esperado := 15
-
-		if esperado != resultado {
-			t.Errorf("resultado %d, esperado %d, dado %v", resultado, esperado, numeros)
-		}
-	})
+	//t.Helper()
 
 	t.Run("coleção de qualquer tamanho", func(t *testing.T) {
 		numeros := []int{1, 2, 3}
@@ -24,4 +16,14 @@ func TestSoma(t *testing.T) {
 			t.Errorf("resultado %d, esperado %d, dado %v", resultado, esperado, numeros)
 		}
 	})
+}
+
+func TestSomaTudo(t *testing.T) {
+
+	recebido := SomaTudo([]int{1, 2}, []int{0, 9})
+	esperado := []int{3, 9}
+
+	if !reflect.DeepEqual(recebido, esperado) {
+		t.Errorf("recebido %v esperado %v", recebido, esperado)
+	}
 }
