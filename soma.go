56d7ded37c9218ebb8353266ee537b9eb72668bf
Ricardo César de Oliveira <rioliveira@bionexo.com>
2020-02-09 02:50:57 -0300

    Code Refactor

--- soma.go
+++ soma.go
@@ -17,3 +17,17 @@ func SomaTudo(numerosParaSomar ...[]int) []int {
 
 	return somas
 }
+
+func SomaTodoOResto(numerosParaSomar ...[]int) []int {
+	var somas []int
+	for _, numeros := range numerosParaSomar {
+		if len(numeros) == 0 {
+			somas = append(somas, 0)
+		} else {
+			final := numeros[1:]
+			somas = append(somas, Soma(final))
+		}
+	}
+
+	return somas
+}
--- soma_test.go
+++ soma_test.go
@@ -27,3 +27,28 @@ func TestSomaTudo(t *testing.T) {
 		t.Errorf("recebido %v esperado %v", recebido, esperado)
 	}
 }
+
+func TestSomaTodoOResto(t *testing.T) {
+
+	verificaSomas := func(t *testing.T, resultado, esperado []int) {
+		t.Helper()
+		if !reflect.DeepEqual(resultado, esperado) {
+			t.Errorf("resultado %v, esperado %v", resultado, esperado)
+		}
+	}
+
+	t.Run("faz as somas de alguns slices", func(t *testing.T) {
+		resultado := SomaTodoOResto([]int{1, 2}, []int{0, 9})
+		esperado := []int{2, 9}
+
+		verificaSomas(t, resultado, esperado)
+	})
+
+	t.Run("soma slices vazios de forma segura", func(t *testing.T) {
+		resultado := SomaTodoOResto([]int{}, []int{3, 4, 5})
+		esperado := []int{0, 9}
+
+		verificaSomas(t, resultado, esperado)
+	})
+
+}
